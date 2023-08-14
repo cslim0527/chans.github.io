9bea315553f0e62f75abdc1982f5c090dac9f876
limchansoo <cslim0527@gmail.com>
2023-08-15 08:44:17 +0900

    refactor: 불필요한 코드 주석

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,15 @@ import StyledComponentsRegistry from "./registry";
 import Header from "@/components/Header";
 import "@/styles/fonts.css";
 import "@/styles/global.css";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const isShowHeader = !(pathname.indexOf("info") > -1);
-  console.log(pathname);
+  // const pathname = usePathname();
+  // const isShowHeader = !(pathname.indexOf("info") > -1);
   return (
     <html>
       <body>
